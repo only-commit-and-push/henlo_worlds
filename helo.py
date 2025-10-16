bc9733e09cdb1134875c3d970265631fc17fe5f6
does. not. matter. <238563967+only-commit-and-push@users.noreply.github.com>
2025-10-16 20:34:12 -0300

    learnding!

--- helo.py
+++ helo.py
@@ -1,6 +1,15 @@
 print("helo world!")
 print("i'm learnding!")
 
+"""
 while True:
     print("this is loop")
+"""
+
+class Greeter:
+    def send_message(self):
+        print("vibe coding is great and easy!!")
+
+m = Greeter()
+m.send_message()
     
